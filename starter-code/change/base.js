34f9d9f0b0c3ffa9258e729e5a7209a0d2046330
Ronak Singh <rssobti@outlook.com>
2017-03-09 15:14:46 -0600

    hw

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,34 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $('.ADD').on('click',function(){
+    // $('.symbol').text() === "Go!" )
+    var left = parseInt($('#left').val());
+    var right = parseInt($('#right').val());
+    var total = left + right;
+  $('#total').val(total);
+  });
+
+  $('.SUBTRACT').on('click',function(){
+    var left = parseInt($('#left').val());
+    var right = parseInt($('#right').val());
+    var total = left - right;
+  $('#total').val(total);
+  });
+
+  $('.MULTIPLY').on('click',function(){
+    var left = parseInt($('#left').val());
+    var right = parseInt($('#right').val());
+    var total = left * right;
+  $('#total').val(total);
+  });
+
+  $('.DIVIDE').on('click',function(){
+    var left = parseInt($('#left').val());
+    var right = parseInt($('#right').val());
+    var total = left / right;
+  $('#total').val(total);
+  });
+
+
 })
